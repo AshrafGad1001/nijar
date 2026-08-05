f2993f412e927fda5cbc45387f00b5dfe26e51cc
AshrafGad1001 <ashrafgad542@gmail.com>
2026-08-05 04:01:14 +0300

    fix: remove item prop from Grid to prevent console warnings

--- src/components/public/AboutContact.tsx
+++ src/components/public/AboutContact.tsx
@@ -13,7 +13,7 @@ export default function AboutContact() {
       <Container maxWidth="lg">
         <Grid container spacing={6}>
           {/* About Section */}
-          <Grid item xs={12} md={6}>
+          <Grid xs={12} md={6}>
             <Box sx={{ mb: 4 }}>
               <Typography variant="h4" sx={{ fontWeight: 900, color: '#1B3A4B', mb: 3 }}>
                 عن ورشة Nijar
@@ -51,7 +51,7 @@ export default function AboutContact() {
           </Grid>
 
           {/* Map Section */}
-          <Grid item xs={12} md={6}>
+          <Grid xs={12} md={6}>
             <Paper 
               elevation={0}
               sx={{ 
