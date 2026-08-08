4a10b301655a85d1ffa065054fd96039b5f73063
AshrafGad1001 <ashrafgad542@gmail.com>
2026-08-08 04:16:18 +0300

    style(frontend): hide spin buttons globally on number inputs

--- src/theme/ThemeRegistry.tsx
+++ src/theme/ThemeRegistry.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import theme from './theme';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
@@ -14,6 +15,17 @@ export default function ThemeRegistry({ children }: { children: React.ReactNode 
     <AppRouterCacheProvider options={{ key: 'muirtl', stylisPlugins: [prefixer, rtlPlugin] }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <GlobalStyles
+          styles={{
+            'input[type=number]::-webkit-inner-spin-button, input[type=number]::-webkit-outer-spin-button': {
+              WebkitAppearance: 'none',
+              margin: 0,
+            },
+            'input[type=number]': {
+              MozAppearance: 'textfield',
+            },
+          }}
+        />
         {children}
       </ThemeProvider>
     </AppRouterCacheProvider>
